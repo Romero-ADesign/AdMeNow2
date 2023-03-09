16cfa278d2ea6a956dd4462fb562d390f00393c4
Adam Romero <aromero0904@gmail.com>
2023-03-09 03:47:12 -0500

    Added Validations for Ad, Tag, User

--- backend/app/models/ad.rb
+++ backend/app/models/ad.rb
@@ -1,4 +1,9 @@
 class Ad < ApplicationRecord
     belongs_to :user
     belongs_to :tag
+
+    validates :name, presence: true
+    validates :description, presence: true, length: { in: 5..250, message: "Description must be at least 5 characters long, 250 characters maximum." }
+    validates :image, presence: true
+    validates :price, presence: true
 end
--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
     has_many :ads
     has_many :tags, through: :ads
+
+    validates :name, presence: true
+    validates :password, presence: true, length: { in: 5..18 }
+    validates :phone_number, presence: true, length:{ is: 12 }, unless: ->(user) { user.email_address.present?}
+    validates :email_address, presence: true, unless: ->(user) { user.phone_number.present?}
+    validates :state, presence: true
 end
