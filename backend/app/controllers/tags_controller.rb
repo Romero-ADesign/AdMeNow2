ef648c352cf113f7f367cb430f458f35083f6309
Adam Romero <aromero0904@gmail.com>
2023-03-10 15:28:37 -0500

    Add Hunter Changes Backend

--- backend/app/controllers/tags_controller.rb
+++ backend/app/controllers/tags_controller.rb
@@ -1,6 +1,8 @@
 class TagsController < ApplicationController
 rescue_from ActiveRecord::RecordInvalid, with: :invalid_tag
 protect_from_forgery with: :null_session
+skip_before_action :authorize
+skip_before_action :verify_authenticity_token
 
     def index
         tag = Tag.all
